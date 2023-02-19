6213433e540c9e02240226b884eb1c480104f782
Vitaliy12224938833 <vitalikd247@gmail.com>
2023-02-19 22:53:28 +0200

    render videos from API

--- react-project/src/assets/pages/Singlepage.jsx
+++ react-project/src/assets/pages/Singlepage.jsx
@@ -3,6 +3,7 @@ import { generateURL } from '../../API/generate-url';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import YouTube from 'react-youtube';
+import '../../styles/Singlepage.css';
 
 export const Singlepage = () => {
   const { id, content } = useParams();
@@ -12,5 +13,28 @@ export const Singlepage = () => {
   const [videosData] = useGetRequest(
     `https://api.themoviedb.org/3/${content}/${id}/videos?api_key=1f63914a91cb95d33f7d8d413f4c28ca&language=en-US`
   );
-  console.log(videosData);
+  return (
+    <div>
+      {pageData && videosData && (
+        <div className='conteiner'>
+          {videosData.results.map((item) => {
+            const { key, official, type } = item;
+            if ((official, type === 'Trailer'))
+              return (
+                <YouTube
+                  key={key}
+                  videoId={key}
+                  opts={{
+                    playerVars: {
+                      autoplay: 1,
+                    },
+                  }}
+                  iframeClassName='trailer'
+                />
+              );
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
