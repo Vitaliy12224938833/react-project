bc2b1937e7def4030799174baad526a9be5a6432
Vitaliy12224938833 <vitalikd247@gmail.com>
2023-02-26 02:32:30 +0200

    removed title from horizontal list

--- react-project/src/pages/Singlepage.jsx
+++ react-project/src/pages/Singlepage.jsx
@@ -83,13 +83,13 @@ export const Singlepage = () => {
         <HorizontalList data={recommendations} title='Recommendations'>
           {(data, listRef, className) => (
             <List className={className} data={data} listRef={listRef}>
-              {(id, title, poster_path, name) => (
+              {(id, title, poster_path) => (
                 <ListItem
                   key={id}
                   className={'horizontal-list-item'}
                   id={id}
                   img={poster_path}
-                  name={title || name}
+                  title={title}
                   categories={[content, category]}
                 />
               )}
@@ -101,13 +101,13 @@ export const Singlepage = () => {
         <HorizontalList data={similarList} title='Similar'>
           {(data, listRef, className) => (
             <List className={className} data={data} listRef={listRef}>
-              {(id, title, poster_path, name) => (
+              {(id, title, poster_path) => (
                 <ListItem
                   key={id}
                   className={'horizontal-list-item'}
                   id={id}
                   img={poster_path}
-                  name={title || name}
+                  title={title}
                   categories={[content, category]}
                 />
               )}
