64016a01c4c982f9aa998027c9554b3699570fd5
Vitaliy12224938833 <vitalikd247@gmail.com>
2023-03-09 20:04:51 +0200

    styled hover effect on slider

--- react-project/src/components/Sliders/Slider.jsx
+++ react-project/src/components/Sliders/Slider.jsx
@@ -5,12 +5,41 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+
 import 'react-multi-carousel/lib/styles.css';
 
 export const Slider = ({ data, children }) => {
   if (!data.length) return;
 
   const [currIdx, setCurrIdx] = useState(0);
+
+  const switchBtnSx = (side) => ({
+    position: 'absolute',
+    zIndex: 666,
+    height: 300,
+    [side]: -20,
+    bottom: '39%',
+    opacity: 0,
+    borderRadius: 5,
+    '&:hover': {
+      backgroundColor: 'rgba(0, 0, 0,  0.3)',
+    },
+  });
+
+  const dotsSx = {
+    textAlign: 'center',
+    opacity: 0,
+  };
+
+  const conteinerSx = {
+    position: 'relative',
+    '&:hover': {
+      '& .MuiIconButton-root, .css-1ddpyth ': {
+        opacity: 1,
+        transition: 'all .3s',
+      },
+    },
+  };
 
   const prevSlide = () => {
     currIdx >= 1 ? setCurrIdx(currIdx - 1) : setCurrIdx(data.length - 1);
@@ -23,41 +52,24 @@ export const Slider = ({ data, children }) => {
   const goToSlide = (idx) => setCurrIdx(idx);
 
   const LeftButton = () => (
-    <IconButton
-      sx={{
-        position: 'absolute',
-        zIndex: 666,
-        height: 300,
-        left: 0,
-        bottom: '39%',
-      }}
-      onClick={prevSlide}
-    >
+    <IconButton sx={switchBtnSx('left')} onClick={prevSlide}>
       <ArrowBackIosNewIcon color={'primary'} sx={{ fontSize: 150 }} />
     </IconButton>
   );
   const RigthButton = () => (
-    <IconButton
-      sx={{
-        position: 'absolute',
-        zIndex: 666,
-        height: 300,
-        right: 0,
-        bottom: '39%',
-      }}
-      onClick={nextSlide}
-    >
+    <IconButton sx={switchBtnSx('right')} onClick={nextSlide}>
       <ArrowForwardIosIcon color={'primary'} sx={{ fontSize: 150 }} />
     </IconButton>
   );
+
   return (
-    <Box sx={{ position: 'relative' }}>
+    <Box sx={conteinerSx}>
       <LeftButton />
       <RigthButton />
-      {children(data[currIdx], 'slideStyles')}
-      <Box sx={{ textAlign: 'center' }}>
+      {children(data[currIdx])}
+      <Box sx={dotsSx}>
         {data.map((item, idx) => (
-          <IconButton onClick={() => goToSlide(idx)}>
+          <IconButton key={idx} onClick={() => goToSlide(idx)}>
             {currIdx === idx ? (
               <RadioButtonCheckedIcon fontSize='small' />
             ) : (
--- react-project/src/components/Video/AllVidoeClips.jsx
+++ react-project/src/components/Video/AllVidoeClips.jsx
@@ -2,12 +2,16 @@ import { Slider } from '../Sliders/Slider';
 import { VideoTrailler } from './YouTobeVideo';
 import { Box } from '@mui/system';
 import { Typography } from '@mui/material';
+import { Container } from '@mui/material';
 
 export const AllVidoeClips = ({ data }) => {
   if (DataTransferItem)
     return (
       <Box>
-        <Typography variant='h4'>Clips</Typography>
+        <Container maxWidth='xl'>
+          <Typography variant='h4'>Clips</Typography>
+        </Container>
+
         <Slider data={data}>
           {(item) => <VideoTrailler data={item} autoplay={0} />}
         </Slider>
